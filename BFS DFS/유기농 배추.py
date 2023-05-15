37c1e62172993d08adde91dbbf1f2ca40f02c3ea
09^_^ <87968927+haaland09009@users.noreply.github.com>
2023-05-15 17:43:20 +0900

    Update 유기농 배추.py

--- BFS DFS/유기농 배추.py
+++ BFS DFS/유기농 배추.py
@@ -41,6 +41,52 @@ for _ in range(t):
             if graph[i][j] == 1: # 그래프 순회 중에 배추가 심어진 곳에 도달한다면 bfs 실행
                 answer.append(bfs(i,j))   
     print(len(answer))                 
+    
+    
+    
+    
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+
+# dx = [0,0,-1,1]
+# dy = [-1,1,0,0]
+
+# def bfs(x,y):
+#     global cnt
+#     q = deque()
+#     q.append((x,y))
+#     graph[x][y] = 0
+#     cnt = 1
+#     while q:
+#         x,y = q.popleft()
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if nx < 0 or nx >= m or ny < 0 or ny >= n:
+#                 continue
+#             if graph[nx][ny] == 1:
+#                 cnt += 1
+#                 graph[nx][ny] = 0
+#                 q.append((nx,ny))
+#     return cnt            
+
+
+# t = int(input())
+# for _ in range(t):
+#     answer = []
+#     m,n,k = map(int,input().split())
+#     graph = [[0]*n for _ in range(m)]
+#     for _ in range(k):
+#         x,y = map(int,input().split())
+#         graph[x][y] = 1
+#     for i in range(m):
+#         for j in range(n):
+#             if graph[i][j] == 1:
+#                 answer.append(bfs(i,j))
+#     print(len(answer))        
+   
+    
 
 
 
